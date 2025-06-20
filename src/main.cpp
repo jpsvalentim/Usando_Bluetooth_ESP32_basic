e792a6713bf9ae0cb94b53e081fbd861502e4e78
João Pedro Silva Valentim <j.pedro__@hotmail.com>
2025-06-20 15:41:19 -0300

    primeiro cod
    teste codigo generico bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
-#include <Arduino.h>
+#include "BluetoothSerial.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Cria o objeto BluetoothSerial
+BluetoothSerial ESP_BT;
+
+#define LED_PIN 2  // GPIO2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  ESP_BT.begin("ESP32_LED_CONTROL"); // Nome do dispositivo Bluetooth
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.println("Bluetooth iniciado. Aguardando comandos...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (ESP_BT.available()) {
+    String command = ESP_BT.readStringUntil('\n');
+    command.trim(); // remove espaços e quebras
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.println("Recebido: " + command);
+
+    if (command == "ON") {
+      digitalWrite(LED_PIN, HIGH);
+      ESP_BT.println("LED ligado!");
+    }
+    else if (command == "OFF") {
+      digitalWrite(LED_PIN, LOW);
+      ESP_BT.println("LED desligado!");
+    }
+    else {
+      ESP_BT.println("Comando inválido!");
+    }
+  }
 }
